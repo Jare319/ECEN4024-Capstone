2b843425c6d7909f1f4ddcb4fae007420bb467ee
Jarrett Crump <jarrettcrump@gmail.com>
2024-04-07 14:26:26 -0500

    IPCManager stuff

--- OceanMotion Control Center/Program.cs
+++ OceanMotion Control Center/Program.cs
@@ -5,7 +5,16 @@ namespace OceanMotion;
 public static class Program
 {
     static void Main(String[] args) {
-        FileHandler fh = new FileHandler();
-        Console.WriteLine(fh.ValidateFile(".\\testData1.txt", 10, 0, 2, 0.0001));
+        IPCManager server = new IPCManager();
+        server.RunAsnyc();
+        // Subscribe to IPCManager event
+
+        //FileHandler fh = new FileHandler();
+        //Console.WriteLine(fh.ValidateFile(".\\testData1.txt", 10, 0, 2, 0.0001));
+        bool ExitFlag = false;
+
+        while (!ExitFlag) {
+
+        }
     }
 }
